77ddd5741c8446695acb20c5a2a396f12d46c476
vedanthv-cc <vedanth.v@codecraft.co.in>
2024-09-19 12:14:50 +0530

    image uploading error resolved

--- app/api/uploadthing/book-route.ts
+++
@@ -1,7 +0,0 @@
-import { createRouteHandler } from "uploadthing/next";
-import { bookCoverRouter } from "./core";
-
-// Routes for book cover uploads
-export const { GET, POST } = createRouteHandler({
-  router: bookCoverRouter,
-});
--- app/api/uploadthing/profile-route.ts
+++
@@ -1,7 +0,0 @@
-import { createRouteHandler } from "uploadthing/next";
-import { profileImageRouter } from "./core";
-
-// Routes for profile image uploads
-export const { GET, POST } = createRouteHandler({
-  router: profileImageRouter,
-});
